9f39990aeff670012779a8d1c40abb22406a28f8
buridan2 <delaney.dvorsky@gmail.com>
2023-11-02 21:01:37 -0400

    pls

--- alphas/alpha_functions.py
+++ alphas/alpha_functions.py
@@ -10,10 +10,18 @@ Normal alphas will use the following naming comvention:
 from __future__ import annotations
 
 import pandas as pd
-
 import pandas_ta as ta
+from alpha_utils import bscore
+from alpha_utils import covariance
+from alpha_utils import csscale
+from alpha_utils import delta
+from alpha_utils import mean
+from alpha_utils import neg
+from alpha_utils import returns
+from alpha_utils import std_a
+from alpha_utils import ts_min
+from alpha_utils import ts_rank
 
-from alpha_utils import *
 
 def alpha_001(data: pd.DataFrame, ticker: str) -> pd.DataFrame:
     """
@@ -33,45 +41,50 @@ def alpha_001(data: pd.DataFrame, ticker: str) -> pd.DataFrame:
     raw_signal.loc[drop_signal_indices] = 0
     return raw_signal
 
+
 def alpha_006(data: pd.DataFrame) -> pd.Series:
     """
     div{inv[delta(bscore(90),14)],tsrank(delta{bscore(13),36},38)}
     """
-    neg_bscore_delta = neg(delta(bscore(data['Adj Close'], 90), 14))
-    tsrank_bscore_delta = ts_rank(delta(bscore(data['Adj Close'], 13), 36), 38)
+    neg_bscore_delta = neg(delta(bscore(data["Adj Close"], 90), 14))
+    tsrank_bscore_delta = ts_rank(delta(bscore(data["Adj Close"], 13), 36), 38)
 
-    return neg_bscore_delta/tsrank_bscore_delta
-    
+    return neg_bscore_delta / tsrank_bscore_delta
+
+
 def alpha_007(data: pd.DataFrame) -> pd.DataFrame:
     """
     inv{mult(rank[tsrank{close,10}], rank[div(close,open)])}
     """
-    rank_tsrank_close = ts_rank(data['Adj Close'], 10).rank()
-    rank_close_open_div = (data['Adj Close'] / data['Open']).rank()
+    rank_tsrank_close = ts_rank(data["Adj Close"], 10).rank()
+    rank_close_open_div = (data["Adj Close"] / data["Open"]).rank()
 
     return neg(rank_tsrank_close * rank_close_open_div)
+
 
 def alpha_008(data: pd.DataFrame) -> pd.DataFrame:
     """
     tsmin(inv[mult{cov(high,returns(30),41),csscale(volume)}], 13)
     """
-    cov = covariance(data['High'], returns(data['Adj Close'], 30), 41)
-    csscale_vol = csscale(data['Volume']) 
+    cov = covariance(data["High"], returns(data["Adj Close"], 30), 41)
+    csscale_vol = csscale(data["Volume"])
 
     return ts_min(neg(cov * csscale_vol), 13)
+
 
 def alpha_009(data: pd.DataFrame) -> pd.DataFrame:
     """
     inv(sum{mean[cov(adv(67),volatility,32),14],56})
     """
-    cov = covariance(ta.adx(data['High'], data['Low'], data['Adj Close'], length=67)['ADX_67'], std_a(data,25), 32)
+    cov = covariance(ta.adx(data["High"], data["Low"], data["Adj Close"], length=67)["ADX_67"], std_a(data, 25), 32)
 
     return neg(sum(mean(cov, 14), 56))
+
 
 def alpha_010(data: pd.DataFrame) -> pd.DataFrame:
     """
     inv{rank(cov[rank(close),rank(volume),3])}
-    """ 
-    cov_ranked = covariance(data['Adj Close'].rank(), data['Volume'].rank(), 3) 
+    """
+    cov_ranked = covariance(data["Adj Close"].rank(), data["Volume"].rank(), 3)
 
     return neg(cov_ranked.rank())
