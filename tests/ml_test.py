12e3c97718bf6335e89cb694f276e7541cd6202f
Anthony G <givansanthony702@gmail.com>
2024-02-07 05:50:19 -0500

    additions

--- tests/ml_test.py
+++ tests/ml_test.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 import math
 
+import time
+
 from sklearn import datasets
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
@@ -28,3 +30,20 @@ def test_lin_reg_returns_reasonable_loss() -> None:
     regressor_mse = linear_regression.mse(y_test, regressor_pred)
 
     assert math.isclose(lin_reg_mse, regressor_mse, rel_tol=0.02)
+
+def test_lin_reg_runtime() -> None:
+    X, y = datasets.make_regression(n_samples=100, n_features=3, noise=20, random_state=17)
+    sgd_start_time = time.time()
+    regressor = linear_regression(learning_rate=1e-3, iters=10_000)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=17)
+    regressor.fit(X_train, y_train)
+    sgd_end_time = time.time()
+    sgd_time = sgd_end_time - sgd_start_time
+    closed_start_time = time.time()
+    regressor = linear_regression(method = "Closed Form")
+    regressor.fit(X,y)
+    closed_end_time = time.time()
+    closed_time = closed_end_time - closed_start_time
+    print(f"SGD Time: {sgd_time} \t Closed Form Time: {closed_time} \t")
+
+test_lin_reg_runtime()
